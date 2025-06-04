81f51fd3e650167202f1d7cc1030451bc2b9d4f9
crisedulo <53383204+crisedulo@users.noreply.github.com>
2025-06-04 15:59:49 -0600

    feat: add validation messages and graphql filter (#8)

--- src/users/application/dto/find-user-by-email.input.ts
+++ src/users/application/dto/find-user-by-email.input.ts
@@ -4,8 +4,8 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 @InputType()
 export class FindUserByEmailInput {
   @Field()
-  @IsEmail()
-  @IsString()
-  @IsNotEmpty()
+  @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  @IsString({ message: 'El correo electrónico debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El correo electrónico es obligatorio' })
   email: string;
 }

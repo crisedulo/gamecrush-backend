ff221ece95d9df2b091de2ff79938007640ddc28
Cristian Eduardo López <crisedulo@gmail.com>
2025-06-04 16:11:48 -0600

    fix: ajustes en updateGame resolver, DTO y validación

--- src/games/application/dto/update-game.input.ts
+++ src/games/application/dto/update-game.input.ts
@@ -3,9 +3,6 @@ import { IsString, IsOptional, IsUrl, IsInt, Min, Max } from 'class-validator';
 
 @InputType()
 export class UpdateGameInput {
-  @Field(() => ID)
-  id: string;
-
   @Field({ nullable: true })
   @IsOptional()
   @IsString({ message: 'El nombre del juego debe ser una cadena de texto' })
@@ -34,6 +31,8 @@ export class UpdateGameInput {
   @IsOptional()
   @IsInt({ message: 'El año de lanzamiento debe ser un número entero' })
   @Min(1970, { message: 'El año de lanzamiento no puede ser menor a 1970' })
-  @Max(new Date().getFullYear() + 5, { message: 'El año de lanzamiento es demasiado alto' })
+  @Max(new Date().getFullYear() + 5, {
+    message: 'El año de lanzamiento es demasiado alto',
+  })
   releaseYear?: number;
 }
